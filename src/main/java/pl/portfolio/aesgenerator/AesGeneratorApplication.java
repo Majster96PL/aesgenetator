f744d1a5c6d07a95f56f292f5c7dcf92193215fa
Majster96PL <piotruniok96@gmail.com>
2022-03-22 18:08:14 +0100

    Initial commit

---
+++ src/main/java/pl/portfolio/aesgenerator/AesGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.portfolio.aesgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/portfolio/aesgenerator/AesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.portfolio.aesgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
