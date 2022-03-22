1e49b63a432b5098b77e2c1f0f5f4476362f7fc1
Majster96PL <piotruniok96@gmail.com>
2022-03-22 19:57:28 +0100

    Create key class to AES algorithm

---
+++ src/main/java/pl/portfolio/aesgenerator/model/algorithm/AESKey.java
@@ -0,0 +1,22 @@
+package pl.portfolio.aesgenerator.model.algorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class AESKey {
+
+    public static void setKey(final String aesKey){
+        try{
+            byte[] key = aesKey.getBytes(StandardCharsets.UTF_8);
+            var messageDigest = MessageDigest.getInstance("SHA-1");
+            key = messageDigest.digest(key);
+            key = Arrays.copyOf(key, 16);
+            var keySpec = new SecretKeySpec(key, "AES");
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+    }
+}
