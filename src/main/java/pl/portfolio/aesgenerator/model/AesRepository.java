bc9a459e79ea33d7e01f30241e67e209d893c261
Majster96PL <piotruniok96@gmail.com>
2022-03-22 19:02:17 +0100

    Create Repository class

---
+++ src/main/java/pl/portfolio/aesgenerator/model/AesRepository.java
@@ -0,0 +1,15 @@
+package pl.portfolio.aesgenerator.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AesRepository extends JpaRepository<Aes, Long> {
+
+    Optional<Aes> findById(Long Id);
+
+}
