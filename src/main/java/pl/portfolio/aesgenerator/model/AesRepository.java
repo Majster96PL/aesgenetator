2a170f398a83e0d507be28b4b5c39a0700c32b19
Majster96PL <piotruniok96@gmail.com>
2022-03-25 22:38:40 +0100

    Create metod encrypt

--- src/main/java/pl/portfolio/aesgenerator/model/AesRepository.java
+++ src/main/java/pl/portfolio/aesgenerator/model/AesRepository.java
@@ -11,5 +11,4 @@ import java.util.Optional;
 public interface AesRepository extends JpaRepository<Aes, Long> {
 
     Optional<Aes> findById(Long Id);
-
 }
--- src/main/java/pl/portfolio/aesgenerator/model/AesService.java
+++ src/main/java/pl/portfolio/aesgenerator/model/AesService.java
@@ -2,12 +2,22 @@ package pl.portfolio.aesgenerator.model;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.portfolio.aesgenerator.algorithm.AESAlgorithm;
+
+import java.util.Optional;
+import java.util.Scanner;
 
 @Service
 @AllArgsConstructor
-public class AesService {
+public class AesService{
 
     private final AesRepository aesRepository;
 
 
+    public Aes encrypt(String text, String key, String encodeText){
+        String stringToEncrypt = AESAlgorithm.encrypt(encodeText);
+        var aes = new Aes(text, key, stringToEncrypt );
+        System.out.println(stringToEncrypt);
+        return aesRepository.save(aes);
+    }
 }
