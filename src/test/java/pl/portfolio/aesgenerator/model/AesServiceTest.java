c95503f6d5734de6e4ba1a00db9e3a3067c28b1d
Majster96PL <piotruniok96@gmail.com>
2022-03-30 02:09:18 +0200

    Add Test

---
+++ src/test/java/pl/portfolio/aesgenerator/model/AesServiceTest.java
@@ -0,0 +1,16 @@
+package pl.portfolio.aesgenerator.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AesServiceTest {
+
+    @Test
+    void encrypt() {
+    }
+
+    @Test
+    void decrypt() {
+    }
+}
