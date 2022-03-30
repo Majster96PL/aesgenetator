b0cfbf8c9c953f639b45494164ff0f3b15e6a799
Majster96PL <piotruniok96@gmail.com>
2022-03-30 02:15:36 +0200

    Modification test

--- src/test/java/pl/portfolio/aesgenerator/model/AesServiceTest.java
+++ src/test/java/pl/portfolio/aesgenerator/model/AesServiceTest.java
@@ -1,16 +1,23 @@
 package pl.portfolio.aesgenerator.model;
 
 import org.junit.jupiter.api.Test;
+import pl.portfolio.aesgenerator.algorithm.AESAlgorithm;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class AesServiceTest {
 
+    private static String encryptedText;
+
     @Test
     void encrypt() {
+        String text = "test";
+        encryptedText = AESAlgorithm.encrypt(text);
+        System.out.println(encryptedText);
     }
 
     @Test
     void decrypt() {
+        String decryptedText = AESAlgorithm.decrypt(encryptedText);
+        System.out.println(decryptedText);
     }
 }
