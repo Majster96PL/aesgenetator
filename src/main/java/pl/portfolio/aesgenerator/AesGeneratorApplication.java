817fc9861e934fddf2d32ec78039ba740b156828
Majster96PL <piotruniok96@gmail.com>
2022-03-30 02:27:38 +0200

    delete imports

--- src/main/java/pl/portfolio/aesgenerator/AesGeneratorApplication.java
+++ src/main/java/pl/portfolio/aesgenerator/AesGeneratorApplication.java
@@ -2,12 +2,9 @@ package pl.portfolio.aesgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.portfolio.aesgenerator.algorithm.AESAlgorithm;
-import pl.portfolio.aesgenerator.json.JSONDocument;
 import pl.portfolio.aesgenerator.model.AesService;
 import pl.portfolio.aesgenerator.ui.TextGUI;
 
-import java.util.Scanner;
 
 @SpringBootApplication
 public class AesGeneratorApplication {
@@ -19,7 +16,7 @@ public class AesGeneratorApplication {
 
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(AesGeneratorApplication.class, args);
 		new TextGUI(aesService);
 	}
